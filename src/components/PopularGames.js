7044bc9c2e02c05e9dd3a95fb65b13ca70050004
Magdalena Szpitalna <madzikresist@gmail.com>
2023-11-29 18:28:20 +0100

    popular games

--- src/components/PopularGames.js
+++ src/components/PopularGames.js
@@ -1,61 +1,44 @@
 import React, {useEffect, useState} from 'react';
-
+import loading from "../loading.gif"
 
 const PopularGames = () => {
-  const [listGames, setListGames] = useState({});
+  const [listGames, setListGames] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-
-  // const getData  = async () => {
-  //   try {
-  //     const response = await fetch(`http://localhost:8000/games`)
-  //     const json = await response.json()
-  //     console.log(json)
-  //   } catch (err) {
-  //     console.error(err)
-  //   }
-  // }
-  // useEffect(() => getData, [])
   useEffect(() => {
-    const fetchGames = async () => {
+    (async () => {
       try {
-        const response = await fetch(`http://localhost:8000/games`);
-        if (!response.ok) throw Error('Did not recieve expected data');
-        const listGames = await response.json();
-        setListGames(listGames[0]);
-        setFetchError(null);
-        console.log(listGames[0], 'listgames')
-      } catch (err){
-        console.log(err.message);
-      } finally {
-        setIsLoading(false);
+        const response = await fetch("http://localhost:8000/games");
+        const data = await response.json();
+        setListGames(data)
+        console.log("test",data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-    }
-    setTimeout(() => {
-      (async () => await fetchGames())();
-    }, 2000)
+      setIsLoading(false)
+    })()
+
   }, []);
-  //
-  // fetch(api_url)
-  //   .then((res) => res.json())
-  //   .then((rows) => {
-  //     console.log(rows)
-  // });
+
+  if(isLoading){
+    return <img src={loading} alt="Loading..." style={{width: '200px', paddingLeft: "40px" }}
+    />
+  }
   return (
     <div className="containerGamesDashboard">
-      <div className="popularGames">
-        <div className="shopButton">Go to the Store</div>
-        {/*{listGames.map((dataObj, index) =>  {*/}
-        {/*  return (*/}
-        {/*    <></>*/}
-        {/*  )}*/}
-        {/*)}*/}
-        {/*<div className="gameBlur" style={{backgroundImage: `url(${listGames.header_image})`}}></div>*/}
-        {/*<div className="gameDashboard" style={{backgroundImage: `url(${listGames.header_image})`}}/>*/}
-        <div className="popularGameBar">
-          <div className="popularGameTitle">{listGames.name}</div>
-          {/*<div className="popularGamePublisher">{listGames.publishers}</div>*/}
-        </div>
+    {listGames.map((dataObj) =>  {
+    return (
+        <div className="popularGames"  key={dataObj.id}>
+          <div className="shopButton">Go to the Store</div>
+            <div className="gameBlur" style={{backgroundImage: `url(${dataObj.header_image})`}}></div>
+            <div className="gameDashboard" style={{backgroundImage: `url(${dataObj.header_image})`}}/>
+            <div className="popularGameBar">
+              <div className="popularGameTitle">{dataObj.name}</div>
+              {/*<div className="popularGamePublisher">{listGames.publishers}</div>*/}
+            </div>
       </div>
+    )}
+    )}
     </div>
   );
 }
