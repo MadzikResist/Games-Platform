5e676519dea527ffe4d6b75052b3d668d8e8057e
Magdalena Szpitalna <madzikresist@gmail.com>
2023-12-08 17:56:04 +0100

    store

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,8 +1,10 @@
 import Navbar from "./Navbar";
 import "../store.css";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DropDown from "./DropDown";
 import PopularGames from "./PopularGames";
+import loading from "../loading.gif";
+import { Link } from "react-router-dom";
 const Store = () => {
   const options = [
     { value: "recommendations", label: "Recommendations" },
@@ -16,6 +18,32 @@ const Store = () => {
     { value: "titleZA", label: "Title Z-A" },
     { value: "titleZA", label: "Title Z-A" },
   ];
+  const [listGames, setListGames] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("http://localhost:8000/store");
+        const data = await response.json();
+        setListGames(data);
+        console.log("test", data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      setIsLoading(false);
+    })();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <img
+        src={loading}
+        alt="Loading..."
+        style={{ width: "200px", paddingLeft: "40px" }}
+      />
+    );
+  }
 
   return (
     <div className="oneGameContainer">
@@ -42,7 +70,40 @@ const Store = () => {
             </div>
             <DropDown options={genres} />
           </div>
-          <div className="games"></div>
+          <div className="games">
+            <div className="containerGamesStore">
+              {listGames.map((dataObj) => {
+                return (
+                  <Link
+                    to={`/game/${dataObj.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <div className="popularGames" key={dataObj.id}>
+                      <div className="shopButton">Go to the Store</div>
+                      <div
+                        className="gameBlur"
+                        style={{
+                          backgroundImage: `url(${dataObj.header_image})`,
+                        }}
+                      ></div>
+                      <div
+                        className="gameDashboard"
+                        style={{
+                          backgroundImage: `url(${dataObj.header_image})`,
+                        }}
+                      />
+                      <div className="popularGameBar">
+                        <div className="popularGameTitle">{dataObj.name}</div>
+                        <div className="popularGamePublisher">
+                          {dataObj.publishers}
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
