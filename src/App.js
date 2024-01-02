11d58e4a183cfc393b3bae6222e4b38e69236ad5
Magdalena Szpitalna <madzikresist@gmail.com>
2024-01-02 18:29:51 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Dashboard from "./components/Dashboard";
 import OneGame from "./components/OneGame";
 import Store from "./components/Store";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} Dashboard />
         <Route path="/game/:id" element={<OneGame />} OneGame />
         <Route path="/store" element={<Store />} Store />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
